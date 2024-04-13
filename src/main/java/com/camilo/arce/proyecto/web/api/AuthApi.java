1fd349747e28b34819e1b7f607998a585b2544be
Camilojavier <camilojavier3@gmail.com>
2024-04-13 13:39:33 -0400

    added constant paths

--- src/main/java/com/camilo/arce/proyecto/web/api/AuthApi.java
+++ src/main/java/com/camilo/arce/proyecto/web/api/AuthApi.java
@@ -1,7 +1,11 @@
 package com.camilo.arce.proyecto.web.api;
 
 public interface AuthApi extends Api {
+    String REDIRECT_URI = "http://localhost:8080/auth/oidc";
+    String REFERER = "http://localhost:5173/";
     String AUTH_ROUTE =  "/auth";
+    String LOGOUT_ROUTE =  "/logout";
+    String LOGIN_ROUTE =  "/login";
     String OPENID_ROUTE = "/oidc";
     String CODE = "code";
     String STATE = "state";
