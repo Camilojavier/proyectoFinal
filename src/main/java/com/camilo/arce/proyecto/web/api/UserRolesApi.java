6bbc2e21481d426a51841cb51c517e6588acc215
Camilojavier <camilojavier3@gmail.com>
2024-05-01 11:26:02 -0400

    refactored code

--- src/main/java/com/camilo/arce/proyecto/web/api/UserRolesApi.java
+++ src/main/java/com/camilo/arce/proyecto/web/api/UserRolesApi.java
@@ -3,6 +3,5 @@ package com.camilo.arce.proyecto.web.api;
 public interface UserRolesApi extends Api{
     String USER_ROLES_ROUTE = API_VERSION + "/userRoles";
     String USER_ROLES_ID = "/{userRolesId}";
-    String USERS_BY_ROLE_ID = "/role" + ROLE_ID;
-    String ROLES_BY_USER_ID = "/user" + USER_ID;
+    String BY_ROLE_ID = "/role" + ROLE_ID;
 }
